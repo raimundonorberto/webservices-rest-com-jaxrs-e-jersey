2bfbea847a32596b8afa85df9391365efbd69f03
Raimundo Norberto Lameira Junior <raimundo.lameira@tjpa.jus.br>
2021-08-23 14:23:22 -0300

    Aulas 01 a 05

---
+++ src/test/java/br/com/alura/loja/ClientTest.java
@@ -0,0 +1,73 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.Servidor;
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.modelo.Projeto;
+
+public class ClientTest {
+	
+    private HttpServer server;
+
+    @Before
+    public void startaServidor() {
+        this.server = Servidor.inicializaServidor();
+    }
+
+    @After
+    public void mataServidor() {
+        server.stop();
+    }
+	
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+		Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/carrinhos/1").request().get(String.class);
+        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+	
+    @Test
+    public void testaQueBuscarUmProjetoTrazOProjetoEsperado() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/projetos/1").request().get(String.class);
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Minha loja", projeto.getNome());
+    }
+    
+    @Test
+    public void testaQueAdicionaUmCarrinho() {
+    	Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        
+        Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        String xml = carrinho.toXML();
+
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+        Response response = target.path("/carrinhos").request().post(entity);
+        Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
+    }
+    
+
+}
